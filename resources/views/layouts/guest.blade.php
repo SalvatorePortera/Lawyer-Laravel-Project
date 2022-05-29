04164028780bdfafdf833a5e09e43a117124ac92
SalvatorePortera <salvap0rtera1@hotmail.com>
2022-05-29 09:22:53 +0900

    csrf error fix

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,7 @@
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="csrf-token" content="{{ csrf_token() }}">
             <meta name="description" content="{{config('configs')->where('key','site_description')->first()->value}}">
             <link rel="icon" href="{{ asset(config('configs')->where('key','favicon_logo')->first()->value) }}" type="image/png" />
             <title>{{ isset($title) ? $title .' | '. config('configs')->where('key','site_title')->first()->value :  config('configs')->where('key','site_title')->first()->value }}</title>
