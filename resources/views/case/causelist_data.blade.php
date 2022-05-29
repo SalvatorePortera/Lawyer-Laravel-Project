3f797aa7b4e370036fb088dfefa89c9faaa7e5fd
SalvatorePortera <salvap0rtera1@hotmail.com>
2022-05-29 19:36:01 +0900

    dfsaf

--- resources/views/case/causelist_data.blade.php
+++ resources/views/case/causelist_data.blade.php
@@ -15,7 +15,16 @@
             <td>{{ $loop->index + 1 }}</td>
             <td>
                 <b>{{ __('case.Case No.') }}: </b> {{$model->case_category? $model->case_category->name : '' }}/{{$model->case_no}} <br>
-                <a href="{{route('category.case.show', $model->case_category_id)}}"><b>{{ __('case.Category') }}: </b> {{$model->case_category? $model->case_category->name : '' }}</a-->  <br>
+                @if($model->case_category_id)
+                    <a href="{{route('category.case.show', $model->case_category_id)}}"><b>{{ __('case.Category') }}
+                            :
+                        </b> {{$model->case_category? $model->case_category->name : '' }}
+                    </a>
+                @else
+                    <b>{{ __('case.Category') }}: </b>
+                    {{$model->case_category? $model->case_category->name : '' }}
+                @endif
+                <br>
                 <a href="{{ route('case.show', $model->id) }}"><b>{{ __('case.Title') }}: </b>{{ $model->title }}
                 </a>
                 <br>
